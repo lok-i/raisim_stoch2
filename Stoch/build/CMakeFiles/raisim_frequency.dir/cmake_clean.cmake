ab4189aff38882541028b8e55c30d037efbf864f
Lokesh Krishna <lokesha1b2c3@gmail.com>
2020-03-10 02:42:54 +0530

    Add files via upload

---
+++ Stoch/build/CMakeFiles/raisim_frequency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raisim_frequency.dir/speed_test.cpp.o"
+  "raisim_frequency.pdb"
+  "raisim_frequency"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raisim_frequency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
