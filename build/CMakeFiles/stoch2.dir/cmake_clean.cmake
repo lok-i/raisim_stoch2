04c4e23aa207fbf4bdd93b0cb0e76b38eb8a71bf
lok-i <lokesha1b2c3@gmail.com>
2020-03-30 21:56:46 +0530

    uploading as a full fledged project

---
+++ build/CMakeFiles/stoch2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stoch2.dir/Stoch2_walker.cpp.o"
+  "stoch2.pdb"
+  "stoch2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stoch2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
